434fcfe0ef729380c8246727edfd51b04f89c95d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-02 16:21:48 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -11,11 +12,14 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
+
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
+    @@post_count +=1
   end
 
 end
