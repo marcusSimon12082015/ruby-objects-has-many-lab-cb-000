4308b58aabe49121a34756b7201ee1b0116bc421
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-02 16:50:32 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+class Post
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+  end
+  def author_name
+    defined?(self.author.name) ? self.author.name : nil 
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+  def artist_name
+    defined?(self.artist.name) ? self.artist.name : nil
+  end
+end
